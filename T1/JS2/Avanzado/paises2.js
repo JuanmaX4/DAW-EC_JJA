5a148447ac647f84f30138624f9ad8fd1549d1e7
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2021-10-29 21:41:58 +0200

    Update Alpha

--- T1/JS2/Avanzado/paises2.js
+++ T1/JS2/Avanzado/paises2.js
@@ -1,4 +1,5 @@
-var paises = [
+function CreateTableFromJSON() {
+    var paises = [
     {
         "nombre":"Albania",
         "capital":"Tirana"
@@ -80,19 +81,44 @@ var paises = [
         "capital":"Praga"
     },
 ]
-function CreateTableFromJSON() {
-    
     var instanciadorTabla = document.getElementById("tabla");
 
-    //! peligro
     // For donde van todo el json
+    var col = [];
     for (var i = 0; i < paises.length; i++) {
-        instanciadorTabla.innerHTML=`
-        <table style="width:100%">
-        <tr>
-          <th>${paises[i].nombre}</th>
-          <th>${paises[i].capital}</th>
-        </tr>
-          </table> `;
+        for (var key in paises[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
     }
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+
+    // Aqui crea las columnas del json
+
+    var tr = table.insertRow(-1);                   // TABLE columnas
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // Tabla cabecera
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < paises.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = paises[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
 }
